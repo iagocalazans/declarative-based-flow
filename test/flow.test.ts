4029f594087502bdc963bdc2cd0be04f1d2d5a2e
Iago Calazans <iago.calazans@gmail.com>
2023-11-08 14:17:42 -0300

    test: :white_check_mark: add passes tests to draft

--- test/flow.test.ts
+++ test/flow.test.ts
@@ -1,51 +1,77 @@
-
-class Flow {
-    private constructor (private readonly uniqueName: symbol) {}
-
-    get name() {
-        return this.uniqueName
-    }
-
-    static create(name: string) {
-        return new Flow(Symbol(name));
-    }
-}
-
-
-
 class Widget {
     protected constructor (protected readonly uniqueName: symbol, private left?: Widget, private right?: Widget) {}
 
     get name() {
-        return this.uniqueName
+        return this.uniqueName;
     }
 
     // TODO: Must have a validator too?
 
     protected success(widget: Widget) {
-        this.left = widget
+        this.left = widget;
     }
 
     protected failed(widget: Widget) {
-        this.right = widget
+        this.right = widget;
     }
 
     static create(name: string) {
-        return new this(Symbol(name))
+        return new this(Symbol(name));
     }
 
-    execute(a: any) {
-        console.log(this.name);
-        console.log("Payload: %o", a);
-
+    process(a: any) {
         // TODO: Work on validator here in try/catch then move foward to right or left...
+        
+        if (!Reflect.has(a, 'payload')) {
+            const payload = a;
+            a = { payload: payload };
+            Object.freeze(a.payload);
+        }
+
 
         if (this.left) {
-            this.left.execute(a);
+            this.left.process(a);
         }
+
+        return a;
     }
 }
 
+class Flow extends Widget {
+    static create(name: string) {
+        return new Flow(Symbol(name));
+    }
+
+    start(widget: Widget) {
+        super.success(widget);
+
+        return this;
+    }
+
+    end() {
+        return this.process.bind(this);
+    }
+}
+
+function findObjectValue <T extends Record<string, any>>(object: T, keys: [keyof T], index = 0) {
+    if (index < keys.length - 1 && Reflect.has(object, keys[index])) {
+        const prop = Reflect.get(object, keys[index])
+        return findObjectValue(prop, keys, ++index)
+    }
+
+    return object[keys[index]];
+}
+
+function useWhiteLabel<T extends Record<string, any>>(label: string) {
+    const validator = RegExp(/(?<={{).+(?=}})/);
+    const [structure] = validator.exec(label)!;
+    const trimmedStructure = structure.trim();
+    return trimmedStructure.split('.') as unknown as [keyof T];
+}
+
+function processWhiteLabel <T extends Record<string, any>>(label: string, object: T) {
+    return findObjectValue(object, useWhiteLabel<T>(label))
+}
 
 class SetVariable extends Widget {
     private action: {
@@ -61,30 +87,52 @@ class SetVariable extends Widget {
     }
 
     static create(name: string) {
-        return new this(Symbol(name))
+        return new this(Symbol(name));
     }
 
-    afterSet(widget: Widget) {
+    moveTo(widget: Widget) {
         super.success(widget);
+
+        return this
     }
 
-    add(varName: string, whitelabel: string) {
+    variable(varName: string, whitelabel: string) {
         this.action.set = {
             var: varName,
             use: whitelabel
         }
+
+        return this
     }
     
-    async execute(a: any): Promise<any> {
+    async process(a: any): Promise<any> {
         if (!this.action.set.use || !this.action.set.var) {
-            throw new Error('To use SetVariable you must set .add("varName", "{{ whitelabel.to.use }}")')
+            throw new Error('To use SetVariable you must set .add("varName", "{{ whitelabel.to.use }}")');
         }
 
-        // TODO: Implement a magic code to act on payload here!
+        if (!Reflect.has(a, 'variables')) {
+            Reflect.defineProperty(a, 'variables', {
+                configurable: true,
+                enumerable: true,
+                value: {}
+            })
 
-        super.execute(a)
+        }
+
+        Reflect.defineProperty(a.variables, this.action.set.var, {
+            configurable: false,
+            writable: false,
+            enumerable: true,
+            value: processWhiteLabel(this.action.set.use, a)
+        })
+
+        super.process(a);
     }
 }
+
+const amazingSetVariableWidgetOne = SetVariable
+        .create('amazing_set_variable_widget_one')
+        .variable('myVarOne', '{{ payload.act.like.that }}');
 
 describe('Flow specs', () => {
     it('should be created as a class with an unique name', () => {
@@ -94,45 +142,76 @@ describe('Flow specs', () => {
         expect(amazingFlow).toBeInstanceOf(Flow);
         expect(amazingFlow.name.toString()).toBe(uniqueName.toString())
     })
+
+    it('should return the process method as extended function', () => {
+        
+        const flow = Flow.create('amazing_flow').start(amazingSetVariableWidgetOne).end();
+
+        expect(flow.toString()).toBe(Flow.prototype.process.bind(Flow).toString())
+    })
 });
 
 describe('Receives widget\'s', () => {
+    let flow: (a: any) => any;
+    
+    beforeAll(() => {
+        amazingSetVariableWidgetOne.moveTo(
+            SetVariable
+                .create('amazing_set_variable_widget_two')
+                .variable('myVarTwo', '{{ payload.act.like.this.should.be.this }}')
+                .moveTo(
+                    SetVariable
+                        .create('amazing_set_variable_widget_three')
+                        .variable('myVarThree', '{{ payload.act.like.those }}')
+                    )
+            );
+        
+        flow = Flow.create('amazing_flow').start(amazingSetVariableWidgetOne).end();
+    })
+
     it('should create a SetVariable widget', () => {
         const widget = SetVariable.create('amazing_set_variable_widget');
-
         expect(widget).toBeInstanceOf(SetVariable);
     })
     
     it('should create two SetVariable widget\'s', () => {
-        const widgetOne = SetVariable.create('amazing_set_variable_widget_one');
-        const widgetTwo = SetVariable.create('amazing_set_variable_widget_two');
-        const widgetThree = SetVariable.create('amazing_set_variable_widget_three');
+        const payload = flow({
+            act: {
+                like: { 
+                    that: 'that', 
+                    this: { 
+                        should: { 
+                            be: { 
+                                this:'this' 
+                            } 
+                        } 
+                    }, 
+                    those: 'those' 
+                }
+            }
+        });
 
-        
-        widgetOne.add('myVarOne', '{{ payload.act.like.that }}');
-        widgetOne.afterSet(
-            widgetTwo
-        );
-
-        widgetTwo.add('myVarTwo', '{{ payload.act.like.this }}');
-        widgetTwo.afterSet(
-            widgetThree
-        );
-
-        widgetThree.add('myVarThree', '{{ payload.act.like.this }}');
-
-        widgetOne.execute({ payload: "data" });
-    })
+        expect(payload).toMatchObject({
+            payload: {
+                act: {
+                    like: { 
+                        that: 'that', 
+                        this: { 
+                            should: { 
+                                be: { 
+                                    this:'this' 
+                                } 
+                            } 
+                        }, 
+                        those: 'those' 
+                    }
+                }
+            },
+            variables: { 
+                myVarOne: 'that', 
+                myVarTwo: 'this', 
+                myVarThree: 'those' 
+            }
+        });
+    });
 });
-
-
-
-// Criar o Flow.
-// Criar widget first_step
-// Criar widget second_step
-// Criar widget third_step
-// Criar widget last_step
-
-// Construir uma árvore de ações com os elementos.
-
-// Deve retornar uma função que receberá o payload.
